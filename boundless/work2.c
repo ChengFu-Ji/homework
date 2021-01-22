2391e547d40e53291247f326dc353d4d7afeb638
boundlesswalker <0907092300abc@gmail.com>
2021-01-23 07:05:34 +0800

    c語言連線mysql

---
+++ boundless/work2.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <mysql.h>
+
+int main() {
+
+	MYSQL *conn_ptr;
+
+	conn_ptr = mysql_init(NULL);
+
+	if (!conn_ptr)
+	{
+		fprintf(stderr, "mysql_init falied\n");
+		return EXIT_FAILURE;
+	}
+
+	conn_ptr = mysql_real_connect(conn_ptr, "localhost", "root", "wang0907092300", "my_db", 0, NULL, 0);
+
+	if (conn_ptr)
+	{
+		printf("Connection success\n");
+	}
+	else
+	{
+		printf("Connection failed\n");
+	}
+
+	mysql_close(conn_ptr);
+
+	return EXIT_SUCCESS;
+}
