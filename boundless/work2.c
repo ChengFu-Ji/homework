bca829c16941bba9c6827a0c74965ddda709407b
boundlesswalker <77542539+boundlesswalker@users.noreply.github.com>
2021-01-23 07:28:04 +0800

    Update work2.c

--- boundless/work2.c
+++ boundless/work2.c
@@ -3,7 +3,7 @@
 #include <mysql.h>
 
 int main() {
-
+        //定義、初始化、創立連線控制代碼
 	MYSQL *conn_ptr;
 
 	conn_ptr = mysql_init(NULL);
@@ -13,7 +13,7 @@ int main() {
 		fprintf(stderr, "mysql_init falied\n");
 		return EXIT_FAILURE;
 	}
-
+        //建立連接
 	conn_ptr = mysql_real_connect(conn_ptr, "localhost", "root", "wang0907092300", "my_db", 0, NULL, 0);
 
 	if (conn_ptr)
@@ -24,7 +24,7 @@ int main() {
 	{
 		printf("Connection failed\n");
 	}
-
+        //關閉連接
 	mysql_close(conn_ptr);
 
 	return EXIT_SUCCESS;
