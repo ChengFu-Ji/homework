dfe246db05c8147bb88fd2f843fd07716a20706d
sidney-Hung <sidney35792461@gmail.com>
2021-02-05 10:33:21 +0800

    test2 of scanf and fflush

---
+++ hsin/inetd/test2scanf.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char exit[20] = "_exit";
+    char txt[20];
+
+    printf("What's wrong with you ?\n");
+    printf("If you don't have any question. keyin _exit to left.\n");
+     
+    while(1)
+    {
+        printf(">>");
+
+        fflush(stdout); //清空buffer並輸出
+
+        scanf("%19s", txt);
+
+        if(strcmp(exit, txt) == 0)break;
+
+        printf("What is the %s ?\n", txt);
+     }
+
+     return 0;
+ }
+
