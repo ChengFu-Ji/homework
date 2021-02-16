3af5a7554fa0647fbc3e055bbc3b8ec0ada82b73
qsxqsx7711 <qsxqsx7711@gmail.com>
2021-02-16 22:24:34 +0800

    new the del

--- KAI_LUN/Linked_list/linklist.c
+++ KAI_LUN/Linked_list/linklist.c
@@ -11,7 +11,7 @@ struct link
 list * add(char *input, list * current_address);
 list * my_command(char * cmd , char * data , list * current_node);
 void show(list * hand_node);
-void del(list * node);
+void del(char * data,list * node);
 
 int main()
 {
@@ -20,25 +20,33 @@ int main()
 	char *cmd,*input;
 	int count = 0;
 	cmd = (char *)malloc(100);
-	
+	input = (char *)malloc(100);
+
 	init_node->data = NULL;	
 	init_node->next = NULL;
 	current = init_node;
 
-
-	for(int i = 0 ; i < 2 ; i++)
+/*鍵入資料測試    
+ *Note:要測試時要記得把分配給 input 空間那行給 mark 掉
+	for(int i = 0 ; i < 2; i++)
 	{
-			
-		input = (char *)malloc(100);
-		scanf("%s",input);
+		input = (char *)malloc(100);	
+		scanf("%s",input);	
 		current = my_command("ADD",input,current);	
-		show(init_node);
-		free(input);	
-	
+		show(init_node);	
+		free(input);
 	}
+*/
 
+/*新增刪除data測試
+	current = my_command("ADD","test01",current);	
+	current = my_command("ADD","test02",current);	
 
-
+	del("test01",init_node);
+	
+	show(init_node);	
+*/	
+	
 	return 0;
 }
 
@@ -64,20 +72,28 @@ list * add(char *input,list * current_node)
 }
 
 //刪除資料
-void del(list * node)
+void del(char * data, list * node)
 {
-	
 	//想法一 從頭找資料刪除
-	
+	list * node_temp;	
+	node_temp = node;
 
-
-
-
-
+	while(1)
+	{
+		if(strcmp(node->data,data) != 0)
+		{	
+			node_temp = node_temp->next;	
+		}	
+		else
+		{
+			node_temp->data = "";
+			break;
+		}
+	}
 
 }
 
-//顯示 list 未修完
+//顯示 list 
 void show(list * hand_node)
 {
 	list * node_temp;
