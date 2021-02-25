d8a1a0f3f2d627bfac818704a4c422d7fdd03c55
sidney-Hung <sidney35792461@gmail.com>
2021-02-25 20:38:01 +0800

    will be better soon.

--- hsin/Linked_List/structtest.c
+++ hsin/Linked_List/structtest.c
@@ -4,18 +4,47 @@
 
 struct Node{
 	
-	char data[10];
+	char data[101];
 	struct list * next;
 
 }typedef node;
 
+void test(char *a)
+{
+	printf("test:%s\n",a);
+}
+
+
 int main()
 {
-	node * a = (node*)malloc(sizeof(node));	
+	char input[105];
+	char *i;
+	i=input;
+	char po[2] = ",";
+	node *fst = NULL;
+	node *cur = NULL;
+	node *dre = NULL; 
 
-	printf("a->data,len:%d\n",sizeof( a -> data ));
-	scanf("%[^\n]",a -> data);
-	printf("data:%s\n",a -> data);
+	for(int q=0;q<10;q++)
+	{
+		//scanf("%[^\n]",i); scanf到第二次時會不斷的收取空白
+		scanf("%s",i);
+		char *d= strtok(i,po);
+		if(strcmp(d,"test")==0)
+		{		
+			//node * a = (node*)malloc(sizeof(node));	
+			test(strtok(NULL,po));
+		}
+		else if(strcmp(d,"__exit")==0)
+		{
+			break;	
+		}
+		//i=0;
+	
+	//strcpy(a -> data, strtok(NULL,po));
+	//printf("data:%s\n",a -> data);
+
+	}
 
 	return 0;
 }
