a3dcb219323d5116db995d19bb51527986195dd6
qsxqsx7711 <qsxqsx7711@gmail.com>
2021-04-13 08:29:10 +0800

    main.c

--- KAI_LUN/showN/showN.c
+++ KAI_LUN/showN/showN.c
@@ -5,33 +5,32 @@
 int data_n(FILE *fp,char *file_name);
 void file_n_len(char *file_name,int *array);
 
-int main(int argv , char * argc[])
+int main(int argc , char * argv[])
 {
-	
 	FILE *fp1;	
 	int n , seek_temp = 0 , file_check_count = 0;
 	char *data , *input_file_name;
-	int *test_len[argv-1] , which_file;
+	int *test_len[argc-1] , which_file;
 	
 	input_file_name = (char *)malloc(32);
 
 	printf("Now have file :\n ");
 	//將多個檔案中各個資料長度寫入array
-	for(int i = 1 ;i < argv ; i++)
+	for(int i = 1 ;i < argc ; i++)
 	{
-		test_len[i-1] = (int *)malloc(data_n(fp1,argc[i]));
+		test_len[i-1] = (int *)malloc(data_n(fp1,argv[i]));
 		
 		//寫入每筆資料的長度
-		file_n_len(argc[i],test_len[i-1]);
-		printf("\t%s\n",argc[i]);	
+		file_n_len(argv[i],test_len[i-1]);
+		printf("\t%s\n",argv[i]);	
 	}
 	
 	printf("Input the file name : \n");
 	//輸入檔案名稱
 	scanf("%[^\n]",input_file_name);	
-	for(int i = 1 ; i < argv ; i++)
+	for(int i = 1 ; i < argc ; i++)
 	{
-		if(strcmp(input_file_name , argc[i]) == 0)	
+		if(strcmp(input_file_name , argv[i]) == 0)	
 		{
 			which_file  = i;	
 		}	
@@ -40,7 +39,7 @@ int main(int argv , char * argc[])
 			file_check_count++;	
 		}
 	}	
-	if(file_check_count == (argv-1) || *input_file_name == '\n' )
+	if(file_check_count == (argc-1) || *input_file_name == '\n' )
 	{
 		printf("=============================================\n");
 		printf("The file entered by the user does not exist\n");
@@ -48,13 +47,13 @@ int main(int argv , char * argc[])
 		return 1;	
 	}
 	
-	printf("Need n with %s : ",argc[which_file]);
+	printf("Need n with %s : ",argv[which_file]);
 	
 	//輸出第n筆資料
 	scanf("%d",&n);
 	fflush(stdin);	
 	
-	if(n < 0 || n > data_n(fp1,argc[which_file]))	
+	if(n < 0 || n > data_n(fp1,argv[which_file]))	
 	{
 		printf("==========================\n");
 		printf("User input n out of range\n");	
@@ -79,15 +78,14 @@ int main(int argv , char * argc[])
 	}
 	else
 	{
-			
 		printf("Current Seek = %d\n", seek_temp);	
 	}
 	
 	//指定要讀的檔案
-	fp1 = fopen(argc[which_file],"r+");
+	fp1 = fopen(argv[which_file],"r+");
 	fseek(fp1,seek_temp,SEEK_SET);
 	data = (char *)malloc(*(test_len[which_file-1] + n) + 1);	
-
+	
 	fread(data,*(test_len[which_file-1] + n-1),1,fp1);
 	printf("%s\n",data);	
 	
