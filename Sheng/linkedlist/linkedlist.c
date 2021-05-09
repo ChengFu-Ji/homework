5893968c68033ff6eb6a4cee38f540a8ab61ebb1
IceFire-Fox <a7613163@gmail.com>
2021-05-09 15:34:45 +0800

    update linkedlist/linkedlist.c

--- Sheng/linkedlist/linkedlist.c
+++ Sheng/linkedlist/linkedlist.c
@@ -7,26 +7,39 @@ typedef struct node_s {
     char data[100];
     struct node_s *next;
 } Node;
+typedef struct cmd {
+    char cmd[10];
+    char cmdlen;
+    int (*cmdfp) (Node **, char *);
+} fcmd;
 
-void add(Node **, char *);
-void save(Node **, char *);
-void load(Node **, char *);
-void showList(Node **);
-void cleanList(Node **);
-void showN(char *);
+int add(Node **, char *);
 int del(Node **, char *);
+int save(Node **, char *);
+int load(Node **, char *);
+int showList(Node **, char *);
+int cleanList(Node **, char *);
+int showN(Node **, char *);
 
 int main() {
     Node **first;
     char *cmd, cmdspc[105], *addon;
-    int id;
+    fcmd cmds[] = {
+        {"add,", 4, add},
+        {"del,", 4, del},
+        {"save,", 5, save},
+        {"load,", 5, load},
+        {"showlist", 8, showList},
+        {"clean", 5, cleanList},
+        {"show,", 5, showN},
+        {"", 0, NULL}
+    };
 
     cmd = cmdspc;
     addon = (char *)malloc(100);
     first = (Node **)malloc(sizeof(Node *));
     *first = (Node *)malloc(sizeof(Node));
     (*first)->next = NULL;
-    id = 1;
 
     printf("Welcome!\n");
     printf("\ncommands : <add>, <del>, <save>, <load>, <showlist>, <cleanlist>, <show,,>\n");
@@ -38,64 +51,35 @@ int main() {
         printf(">> ");
         fgets(cmd, 105, stdin);
 
-        if (!strncmp(cmd, "add,", 4)) {
-            if (strstr(cmd, "'") != NULL && strstr(strstr(cmd, "'")+1, "'") != NULL) {
-                if (strstr(cmd+4, ",") > strstr(cmd+4, "'") || strstr(cmd+4, ",") == NULL) 
-                    sprintf(addon, "%d,%s", id, cmd+4);
-                else 
-                    strcpy(addon, cmd+4);
-                
-                add(first, addon);
-                id++;
-                printf("Command Success\n");
-            } else {
-                printf("Use '' to encase data\n");
-            }
-
-        } else if (!strncmp(cmd, "del,", 4)) {
-            if (strstr(cmd, "'") != NULL && strstr(strstr(cmd, "'")+1, "'") != NULL || atoi(cmd+4) != 0) {
-                if (!del(first, cmd+4)) 
-                    printf("Command Success\n");
-                else 
-                    printf("Failed to process\n");
-            } else {
-                printf("Use '' to encase data or type in number to del\n");
-            }
-
-        } else if (!strncmp(cmd, "save,", 5)) {
-            save(first, cmd+5);
-            printf("Command Done\n");
-        } else if (!strncmp(cmd, "load,", 5)) {
-            load(first, cmd+5);
-            printf("Command Done\n");
-        } else if (!strncmp(cmd, "show,", 5)) {
-            showN(cmd+5);
-        } else if (!strcmp(cmd, "showlist\n")) {
-            showList(first);
-        } else if (!strcmp(cmd, "cleanlist\n")) {
-            cleanList(first);
-        } else if (!strcmp(cmd, "exit\n")) {
+        int i = 0;
+        while (cmds[i].cmdlen) {
+            if (!strncmp(cmds[i].cmd, cmd, (int)cmds[i].cmdlen)) {
+                if (cmds[i].cmdfp(first, (cmd+(int)cmds[i].cmdlen)) == 0) {
+                    printf("command done!\n");
+                } else {
+                    printf("command fail...\n");
+                }
+            } 
+            i++;
+        }
+        if (!strcmp(cmd, "exit\n")) {
             printf("exiting...\n");
             break;
-        } else {
-            printf("wrong input. Try again!\n");
         }
     }
 
-    cleanList(first);
+    cleanList(first, "");
     free(*first);
     free(first);
     return 0;
 }
 
-void add (Node **node, char *input) {
+int add (Node **node, char *input) {
     Node *new_node, *cur;
     char *data;
 
-    *(strstr(input, "\n")) = '\0'; 
-    *(strstr(strstr(input, "'")+1, "'")) = '\n';
-    data = strstr(input, "'")+1;
-    *(data-1) = '\0';
+    if (atoi(input) == 0 || strstr(input, ",") == NULL)
+        return 1;
 
     cur = *node;
     while (cur->next != NULL)
@@ -104,52 +88,29 @@ void add (Node **node, char *input) {
     new_node = (Node *)malloc(sizeof(Node));
 
     strcpy(new_node->data, input);
-    strcat(new_node->data, data);
     new_node->next = NULL;
 
     cur->next = new_node;
+    return 0;
 }
 
 int del (Node **node, char *input) {
     Node *cur, *tmp;
     char *data;
-    int id;
-
-    id = 0;
-    if (strstr(input, "'") == NULL) { 
-        id = atoi(input);
-    } else {
-        *(strstr(input, "\n")) = '\0'; 
-        *(strstr(strstr(input, "'")+1, "'")) = '\n';
-        data = strstr(input, "'")+1;
-    }
-
 
     cur = *node;
     while (cur->next != NULL) {
-        if (id == 0) {
-            if (strcmp(strstr(cur->next->data, ",")+1, data) != 0) {
-                cur = cur->next;
-                continue;
-            }
-        } else {
-            if (atoi(cur->next->data) != id) {
-                cur = cur->next;
-                continue;
-            }
+        if (!strcmp(cur->next->data, input)) {
+            tmp = cur->next->next;
+            free(cur->next); 
+            cur->next = tmp;
         }
-
-        tmp = cur->next->next;
-        free(cur->next); 
-        cur->next = tmp;
         return 0;
-
     } 
-
     return 1;
 }
 
-void save (Node **node, char *fn) {
+int save (Node **node, char *fn) {
     FILE *save, *index;
     char *index_fn, *data;
     int len, id;
@@ -186,9 +147,10 @@ void save (Node **node, char *fn) {
         free(index_fn);
         fclose(save);
     }
+    return 0;
 }
 
-void load (Node **node, char *fn) {
+int load (Node **node, char *fn) {
     FILE *load;
     char *data, *output;
     int i, len, id;
@@ -222,9 +184,10 @@ void load (Node **node, char *fn) {
         free(data);
         fclose(load);
     }
+    return 0;
 }
 
-void showList (Node **node) {
+int showList (Node **node, char *none) {
     Node *cur;
 
     cur = (*node)->next;
@@ -233,9 +196,10 @@ void showList (Node **node) {
         cur = cur->next;
     }
     printf("----------------------------end----------------------------\n");
+    return 0;
 }
 
-void cleanList (Node **node) {
+int cleanList (Node **node, char *none) {
     Node *cur, *next;
 
     cur = (*node)->next;
@@ -246,33 +210,34 @@ void cleanList (Node **node) {
         cur = next;
     }
     (*node)->next = NULL;
-    printf("Clean Done!\n");
+    return 0;
 }
 
-void showN (char *input) {
+int showN (Node **none, char *input) {
     FILE *show, *index;
     char *index_name, *data;
-    int n, Flen, pos, nextpos, dataId;
+    int n, pos, nextpos, dataId, status;
 
+    status = 1;
     if (strstr(input, ",") != NULL) {
         if ((n = atoi(strstr(input, ",")+1)) != 0) {
 
             *(strstr(input, ",")) = '\0';
             if ((show = fopen(input, "r")) == NULL) 
-                return;
+                return 1;
 
             index_name = (char *)malloc(strlen(input)+4);
+            strcpy(index_name, input);
             if (strstr(index_name, ".") != NULL) 
                 *(strstr(index_name, ".")) = '\0';
             
-            strcpy(index_name, input);
             strcat(index_name, ".idx");
             if ((index = fopen(index_name, "rb")) == NULL)
-                return;
+                return 1;
 
             fseek(index, 0, SEEK_END);
-            Flen = ftell(index);
-            if (Flen/sizeof(int) > n) {
+
+            if (ftell(index)/sizeof(int) > n) {
                 fseek(index, (n-1)*sizeof(int), SEEK_SET); 
                 fread(&pos, sizeof(int), 1, index);
                 fread(&nextpos, sizeof(int), 1, index);
@@ -286,16 +251,12 @@ void showN (char *input) {
                 printf("id = %d, data = %s\n", dataId, data);
                 
                 free(data);
-            } else {
-                printf("error range!\n");
+                status = 0;
             }
             fclose(show);
             fclose(index);
             free(index_name);
-        } else {
-            printf("error range!\n");
-        }
-    } else {
-        printf("wrong input!\n");
-    }
+        } 
+    } 
+    return status;
 }
