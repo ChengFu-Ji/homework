cb84402edee474671d299770482ab39c5742489f
IceFire-Fox <a7613163@gmail.com>
2021-11-05 02:07:21 +0800

    update timeTest/blocking and README.md

---
+++ Sheng/socketWhiteboard/timeTest/blocking/socketServer.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h> //close, read, write
+#include <errno.h> //errno
+#include <poll.h>
+#include <time.h>
+
+#include "linkedlist.h"
+
+#define MAX_P 12 
+
+void *writefile(void*);
+void showTime(char *, int);
+void writeCSV(size_t, char *, int);
+void CSVinit();
+
+int times = 0;
+int main() {
+    int serverSockfd, clientSockfd, serverlen, clientlen;
+    struct sockaddr_in  server, client;
+    struct pollfd clients[MAX_P];
+
+    serverSockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    serverlen = sizeof(server);
+
+    bzero(&server, serverlen);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(2244);
+    server.sin_addr.s_addr = INADDR_ANY;
+
+    bind(serverSockfd, (struct sockaddr *) &server, sizeof(server));
+    listen(serverSockfd, MAX_P);
+
+    clients[0].fd = serverSockfd;
+    clients[0].events = POLLRDNORM;
+    for (int i = 1; i < MAX_P; i++) {
+        clients[i].fd = -1;
+    }
+    CSVinit();
+
+    int nready = 0;
+    int maxi = 1;
+    int Scount = 0;
+    printf("waiting...\n");
+
+    while (1) {
+        nready = poll(clients, maxi, -1);
+
+        if (clients[0].revents & POLLRDNORM) {
+            clientlen = sizeof(client);
+            clientSockfd = accept(serverSockfd, (struct sockaddr *) &client, (socklen_t *) &clientlen);
+            int i = 0;
+            for (i = 1; i <= MAX_P; i++) {
+                if (clients[i].fd < 0) {
+                    clients[i].fd = clientSockfd;
+                    clients[i].events = POLLRDNORM;
+                    showTime("Add", i);
+                    write(clientSockfd, &i, sizeof(int));
+                    break;
+                }
+            }
+            if (i == MAX_P) {
+                printf("There are too many people here, Exiting...\n");
+                exit(1);
+            } else if (i == maxi) {
+                maxi++;
+                printf("i = %d, maxi = %d\n", i, maxi);
+            }
+
+            if (--nready <= 0) {
+                continue;
+            }
+        }
+
+        for (int i = 1; i < maxi; i++) {
+            if (clients[i].fd < 0) {
+                continue;
+            }
+            clientSockfd = clients[i].fd;
+            if (clients[i].revents & (POLLRDNORM | POLLERR)) {
+                char test[100];
+                Data_s tmp;
+                int n;
+
+                if ((n = read(clientSockfd, &tmp, sizeof(tmp))) > 0) {
+                    if (tmp.x == -1 && tmp.y == 0) {
+                        printf("time %d\n", ++times);
+                        showTime("Read", i);
+                    }
+                    
+                    strcpy(test, "12345678")
+                    //模擬轉發給其他使用者
+                    for (int j = 0; j < 9; j++) {
+                        write(clientSockfd, test, strlen(test));
+                    }
+
+                    /*
+                    for (int j = 1; j < maxi; j++) {
+                        if ( clients[j].fd != clientSockfd && clients[j].fd != -1) {
+                            //int n = sprintf(sendArr, "x: %d, y: %d\n", tmp.x, tmp.y);
+                            //write(clients[j].fd, sendArr, n);
+                            //write(clients[j].fd, &tmp, n);
+                            //send(clients[j].fd, sendArr, n, MSG_DONTROUTE | MSG_DONTWAIT); 
+                            write(clients[j].fd, test, k);
+                        }
+                    }
+                    */
+                } else {
+                    if (n == 0 || errno == ECONNRESET) {
+                        showTime("Exit", i);
+                        close(clientSockfd);
+                        clients[i].fd = -1;
+                    }
+                } 
+            }
+            if (--nready <= 0) {
+                continue;
+            }
+        }
+    }
+    return 0;
+}
+
+void showTime(char *status, int id) {
+    struct timespec ts;
+    size_t t;
+
+    clock_gettime(CLOCK_REALTIME, &ts);
+
+    t = ts.tv_sec;
+    for (int i = 0; i < 9; i++) {
+        t *= 10;
+    }
+
+    writeCSV(ts.tv_nsec+t, status, id);
+    printf("%s, time [%lu ns]\n", status, ts.tv_nsec + t);
+}
+
+void writeCSV(size_t time, char *status, int id) {
+    FILE *fp;
+
+    fp = fopen("serverLog.csv", "a");
+    fprintf(fp, "%d, %s, %lu\n", id, status, time);
+    fclose(fp);
+}
+
+void CSVinit() {
+    FILE *fp;
+
+    fp = fopen("serverLog.csv", "w");
+    fprintf(fp, "PID, status, recv_time (unit: ns)\n");
+    fclose(fp);
+}
+
+
---
+++ Sheng/socketWhiteboard/timeTest/blocking/whiteboard.cpp
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <time.h>
+
+#include "linkedlist.h"
+
+using namespace cv;
+
+void showTime (char *);
+void autoTesting (int fd);
+void writeCSV(size_t, char *);
+void CSVinit();
+
+char windowName[] = "Display Image";
+char imageName[] = "background.png";
+Mat image;
+Node_s **sendList;
+int pid = 0;
+
+int main() {
+    FILE *fp;
+    int fd;
+    struct sockaddr_in me;
+    struct hostent *host;
+
+    if ((fp = fopen("clientLog.csv", "r"))) {
+        fclose(fp);
+    } else {
+        CSVinit();
+    }
+    sendList = (Node_s **)malloc(sizeof(Node_s *));
+    *sendList = (Node_s *)malloc(sizeof(Node_s));
+    (*sendList)->next = NULL;
+
+    host = gethostbyname("localhost");
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    memset(&me, 0, sizeof(struct sockaddr_in));
+    me.sin_family = AF_INET;
+    me.sin_port = htons(2244);
+    memcpy(&me.sin_addr, host->h_addr_list[0], host->h_length);
+    
+    if (connect(fd, (struct sockaddr *) &me, sizeof(struct sockaddr_in)) < 0) {
+        printf("Error Connection!\n");
+        exit(1);
+    } else {
+        showTime("connected");
+        read(fd, &pid, sizeof(int));
+    }
+
+    image = imread(imageName, 1);
+    namedWindow(windowName, WINDOW_NORMAL);
+    imshow(windowName, image);
+
+    autoTesting(fd);
+    waitKey(10);
+
+    close(fd);
+    showTime("exit");
+    free(*sendList);
+    free(sendList);
+
+    return 0;
+}
+
+void autoTesting (int fd) {
+    Point sp, ep;
+    char data[100];
+    int count;
+    double spacingX, spacingY;
+
+    srand(time(NULL));
+    for (int count = 1; count <= 100; count++) {
+        sp.x = rand() % 600;
+        sp.y = rand() % 1000;
+        ep.x = rand() % 600;
+        ep.y = rand() % 1000;
+        while (sp.x == ep.x && sp.y == ep.y) {
+            sp.x = rand() % 600;
+            sp.y = rand() % 1000;
+        }
+
+        spacingX = (ep.x - sp.x)/100.0;
+        spacingY = (ep.y - sp.y)/100.0;
+
+        for (int i = 1; i <= 100; i++) {
+            ep.x = sp.x + (int) (spacingX);
+            ep.y = sp.y + (int) (spacingY);
+            line(image, sp, ep, Scalar(0, 255, 0), 2);
+            imshow(windowName, image);
+            add(sendList, sp.x, sp.y);
+            sp = ep;
+        }
+
+        waitKey(1);
+        add(sendList, sp.x, sp.y);
+        add(sendList, -1, 0);
+        
+        socket_write(sendList, fd);
+
+        showTime("LineSend");
+        
+        cleanList(sendList);
+        int i = 0;
+        //當資料送到 第 75 筆，開始讀取 buffer 中的資料
+        if (count >= 75) {
+            for (int j = 0; j < 2000; j++) {
+                read(fd, data, 8);
+            }
+        }
+
+        printf("times %d\n", count);
+    }
+}
+
+void showTime (char *status) {
+    struct timespec ts;
+    size_t t;
+
+    clock_gettime(CLOCK_REALTIME, &ts);
+
+    t = ts.tv_sec;
+    for (int i = 0; i < 9; i++) {
+        t *= 10;
+    }
+
+    writeCSV(ts.tv_nsec+t, status);
+    printf("%s, time[%lu ns]\n", status, t + ts.tv_nsec);
+}
+
+void writeCSV(size_t time, char *status) {
+    FILE *fp;
+
+    while (1) {
+        fp = fopen("clientLog.csv", "a");
+        if (fp) {
+            break;
+        }
+    }
+    fprintf(fp, "%d, %s, %lu\n", pid, status, time);
+    fclose(fp);
+}
+
+void CSVinit() {
+    FILE *fp;
+
+    fp = fopen("clientLog.csv", "w");
+    fprintf(fp, "PID, status, recv_time (unit: ns)\n");
+    fclose(fp);
+}
+
